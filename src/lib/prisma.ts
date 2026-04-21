4ed24e7e8f7c6db62ef06c1236c10e9fa46877ef
Swarup Shekhar <shekharswarup67@gmail.com>
2026-04-21 14:20:26 +0530

    refactor: implement singleton pattern for Prisma client using globalThis and a factory function

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -2,14 +2,18 @@ import { PrismaClient } from "@prisma/client";
 import { PrismaPg } from "@prisma/adapter-pg";
 import { Pool } from "pg";
 
-const globalForPrisma = global as unknown as { prisma: PrismaClient };
+type PrismaClientSingleton = ReturnType<typeof createPrismaClient>;
 
-export const prisma =
-  globalForPrisma.prisma ||
-  (() => {
+function createPrismaClient() {
     const pool = new Pool({ connectionString: process.env.DATABASE_URL });
     const adapter = new PrismaPg(pool);
     return new PrismaClient({ adapter });
-  })();
+}
 
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+const globalForPrisma = globalThis as unknown as {
+    _primsa: PrismaClientSingleton | undefined;
+};
+
+export const prisma = globalForPrisma._primsa ?? createPrismaClient();
+
+if (process.env.NODE_ENV !== "production") globalForPrisma._primsa = prisma;
